9e631931516e327fd40dd3ec66443fad6b656f9e
Adarsha Acharya <aadarshaaacharya@gmail.com>
2026-02-17 15:14:16 +0545

    readme changes

--- app/src/lib/api.ts
+++ app/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with default configuration
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/v1',
+    baseURL: 'http://localhost:8001/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
